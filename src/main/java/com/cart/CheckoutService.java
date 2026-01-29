deb3a79a024ddb448765ecf98e631e9957119aa7
Muhammad Ahmad <rufi_a@hotmail.com>
2026-01-29 10:03:55 +0000

    Step 2 - Refactored to simplify the calculations

--- src/main/java/com/cart/CheckoutService.java
+++ src/main/java/com/cart/CheckoutService.java
@@ -1,26 +1,36 @@
 package com.cart;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 //add logging
 public class CheckoutService {
 
     public BigDecimal calculateTotal(List<Item> items) {
-        return Arrays.stream(Item.values())
-                .map(item -> {
-                    long count = items.stream().filter(i -> i == item).count();
-                    return calculateItemTotal(item, count);
-                })
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        final Map<Item, Long> itemCounts = items.stream()
+                .collect(Collectors.groupingBy(item -> item, Collectors.counting()));
+        final BigDecimal appleTotal = calculateAppleTotal(itemCounts.getOrDefault(Item.APPLE, 0L));
+        final BigDecimal orangeTotal = calculateOrangeTotal(itemCounts.getOrDefault(Item.ORANGE, 0L));
+
+        return appleTotal.add(orangeTotal);
     }
 
-    private BigDecimal calculateItemTotal(Item item, long count) {
-        return switch (item.getOffer()) {
-            case BUY_ONE_GET_ONE_FREE -> item.getPrice().multiply(BigDecimal.valueOf(count / 2 + count % 2));
-            case THREE_FOR_TWO -> item.getPrice().multiply(BigDecimal.valueOf((count / 3) * 2 + (count % 3)));
-            case NONE -> item.getPrice().multiply(BigDecimal.valueOf(count));
-        };
+    private BigDecimal calculateAppleTotal(long quantity) {
+        long pairs = quantity / 2;
+        long singles = quantity % 2;
+        long payableApples = pairs + singles;
+
+        return Item.APPLE.getPrice().multiply(BigDecimal.valueOf(payableApples));
     }
+
+    private BigDecimal calculateOrangeTotal(long quantity) {
+        long setsOfThree = quantity / 3;
+        long remainder = quantity % 3;
+        long payableOranges = (setsOfThree * 2) + remainder;
+
+        return Item.ORANGE.getPrice().multiply(BigDecimal.valueOf(payableOranges));
+    }
+
 }
